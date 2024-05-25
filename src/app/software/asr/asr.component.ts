083e99e26683154ca08d2963649367717211883f
coloz <coloz999@gmail.com>
2024-05-26 01:35:53 +0800

    更新

--- src/app/software/asr/asr.component.ts
+++ src/app/software/asr/asr.component.ts
@@ -1,9 +1,30 @@
 import { Component } from '@angular/core';
+import { MarkdownComponent } from '../../component/markdown/markdown.component';
+import { NzButtonModule } from 'ng-zorro-antd/button';
+import { NzInputModule } from 'ng-zorro-antd/input';
+import { NzInputNumberModule } from 'ng-zorro-antd/input-number';
+import { NzStepsModule } from 'ng-zorro-antd/steps';
+import { FormsModule } from '@angular/forms';
+import { CommonModule } from '@angular/common';
+import { NzCodeEditorModule } from 'ng-zorro-antd/code-editor';
+import { NzSelectModule } from 'ng-zorro-antd/select';
+import { NzSliderModule } from 'ng-zorro-antd/slider';
 
 @Component({
   selector: 'app-asr',
   standalone: true,
-  imports: [],
+  imports: [
+    CommonModule,
+    FormsModule,
+    NzStepsModule,
+    NzInputNumberModule,
+    NzInputModule,
+    NzButtonModule,
+    MarkdownComponent,
+    // NzCodeEditorModule,
+    NzSelectModule,
+    NzSliderModule
+  ],
   templateUrl: './asr.component.html',
   styleUrl: './asr.component.scss'
 })
